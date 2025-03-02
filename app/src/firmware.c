ade4239e8e26ab52bf95cb677a2a2cdbe1021d54
Brxnxvxn <brana.keeth@gmail.com>
2025-03-01 22:48:24 -0500

    ran bootloader on stm

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,10 +1,18 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
 
 #include "core/system.h"
 
 #define LED_PORT        (GPIOA)
 #define LED_PIN         (GPIO5)
+
+#define BOOTLOADER_SIZE  (0x8000U)
+
+static void vector_setup(void) {
+    SCB_VTOR = BOOTLOADER_SIZE;
+}
+
 
 static void gpio_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOA);
@@ -12,6 +20,7 @@ static void gpio_setup(void) {
 }
 
 int main(void) {
+    vector_setup();
     system_setup();
     gpio_setup();
 
